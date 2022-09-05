4e4d78b7d00fbc6177da56da50dc9215f8649279
Gwyn Lockett <gwyn.lockett@guardian.co.uk>
2022-09-05 13:02:40 +0100

    working up to this point

--- django_venv/mysite/main/views.py
+++ django_venv/mysite/main/views.py
@@ -7,7 +7,7 @@ from .models import ToDoList, Item
 # Create your views here.
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    return render(response, "main/base.html", {})
+    return render(response, "main/list.html", {"ls":ls})
 
 #Create a Homepage
 def home(response):
